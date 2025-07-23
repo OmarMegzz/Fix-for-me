d05b3fbebbae683ee4c10c72d062bddad5d7f980
omar magdy <omarmagdyzz12@gmail.com>
2025-07-23 17:37:44 +0300

    initial commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App";
-import Home from "./pages/Home";
-import AboutUs from "./pages/AboutUs";
-import ContactUs from "./pages/ContactUs";
 import "./index.css";
-import FormToEmail from "./components/FormToEmail.jsx";
-
-// Routes using Data Router
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
